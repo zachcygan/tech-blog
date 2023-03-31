dc9c4357987e7890a36063e6e85d20165df65f29
Zachary Cygan <zachcygan@gmail.com>
2023-03-31 15:31:19 -0700

    working on handlebars and html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-sessions');
 const routes = require('controllers')
+const exphbs = require('express-handlebars');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -19,6 +20,8 @@ const sess = {
 };
 
 app.use(session(sess));
+
+app.engine('handlebars', hbs.engine)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
