f8b4bacfc092bd795377b78e41804cba951489e1
Zachary Cygan <zachcygan@gmail.com>
2023-03-31 18:19:21 -0700

    working on homepage handlebars

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err)
+    }
+});
