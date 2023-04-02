43bd7a971602e4df5d749b0ee804dbba982be2c9
Zachary Cygan <zachcygan@gmail.com>
2023-04-01 17:24:59 -0700

    added comments model

--- models/Post.js
+++ models/Post.js
@@ -23,10 +23,6 @@ Post.init(
           allowNull: false,
           defaultValue: DataTypes.NOW,
         },
-        comment: {
-          type: DataTypes.STRING,
-          allowNull: false
-        },
         user_id: {
           type: DataTypes.INTEGER,
           references: {
